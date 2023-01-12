c30af0d109843c74dfc26b30be927d739cadba68
Maher BALDI <maher.baldi@proxym-it.com>
2023-01-12 17:24:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    let message='hello me§§';
+    let random = Math.random();
+    if(random >0.5){
+        message ='it is random'
+    }
+    return <h1 className="wrapper">{message} {random} </h1>
 }
-
-export default App;
+export default App
